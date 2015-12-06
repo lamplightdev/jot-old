3608aa9cc1af5a2e8627fe9fd7369befc14f622f
Chris Haynes (mac) <chris.haynes@gmail.com>
2015-12-06 22:05:55 +0000

    use app.min.js in sw

--- public/serviceworker-es6.js
+++ public/serviceworker-es6.js
@@ -18,6 +18,7 @@ self.addEventListener('install', event => {
           '/',
           '/css/app.css',
           '/js/dist/app.js',
+          '/js/dist/app.min.js',
           '/js/browser-polyfill.js',
           '/js/pouchdb.js',
           // '/js/webfontloader.js',
